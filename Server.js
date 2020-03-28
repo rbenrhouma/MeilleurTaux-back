21e95743e028e5f885325c0fcefd8172621fc9c5
Ridha <rbenrhouma2@gmail.com>
2020-03-28 22:29:52 +0100

    server

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose"); // for db
 const bodyParser = require("body-parser");
-// placer des middlewares
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -9,22 +9,6 @@ mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-
-//let currentDataBase = "Devis";
-
-// mongoose.connect(
-//   process.env.MONGODB_URI,
-//   { useUnifiedTopology: true, useNewUrlParser: true },
-//   err => {
-//     if (!err) {
-//       console.log("Connexion to MongoDB succeeded...");
-//     } else {
-//       console.log(
-//         "Error in DB Connexion: " + JSON.stringify(err, undefined, 2)
-//       );
-//     }
-//   }
-// );
 
 const backOfficeRoute = require("./Routes/backoffice");
 app.use(backOfficeRoute);
@@ -40,5 +24,5 @@ app.all("*", (req, res) => {
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server démarré serveur ${port}`);
+  console.log(`Server démarré serveur ${process.env.PORT}`);
 });
